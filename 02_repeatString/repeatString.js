79c2603df7a445188882fb9e769b4d739a6ec307
owl <174026543+Eternal-Owl@users.noreply.github.com>
2024-10-09 17:05:51 -0500

    ex. 01 and 02 are completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, iterations) {
+    let returnString = "";
+    let i = 0;
+    if (iterations < 0) {
+        returnString = 'ERROR'
+    } else {
+        while ( i < iterations) {
+            returnString += string;
+            i++;
+        }
+    }   
+    return returnString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
